a90f25953200f098c6d150a2f16c499fe5117077
Kishan Takoordyal <kishan@kinesis.games>
2020-10-31 07:21:46 +0400

    create UI for registering

--- src/Screens/LoginForm.js
+++ src/Screens/LoginForm.js
@@ -1,9 +1,92 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import background from '../Assets/images/4048243.jpg';
 import logo from '../Assets/images/logo.png';
 
 export default function LoginForm() {
+  const [isRegistering, setIsRegistering] = useState(false);
+
+  const Login = (
+    <div className="w-5/6 mx-auto">
+      <div className="font-bold tracking-wide text-white sm:text-4xl text-2xl sm:mt-0 mt-4">
+        Login
+      </div>
+      <input
+        type="text"
+        placeholder="Enter Username..."
+        className="w-full p-2 text-gray-100 bg-gray-700 rounded-lg my-4"
+      />
+      <input
+        type="password"
+        placeholder="Enter Password..."
+        className="w-full p-2 text-gray-100 bg-gray-700 rounded-lg mb-4"
+      />
+      <button
+        className="p-2 rounded-lg bg-gray-900 uppercase tracking-wide font-bold text-lg hover:bg-blue-600 focus:bg-blue-600 text-gray-300 sm:w-1/3 w-full mb-2"
+        onClick={() => null}
+      >
+        Let's Go!
+      </button>
+      <div className="sm:text-lg text-md text-gray-400">
+        Don't already have an account?{' '}
+        <button
+          className="underline text-blue-400 sm:mb-0 mb-4"
+          onClick={() => setIsRegistering(true)}
+        >
+          Register Now!
+        </button>
+      </div>
+    </div>
+  );
+
+  const Register = (
+    <div className="w-5/6 mx-auto">
+      <div className="font-bold tracking-wide text-white sm:text-4xl text-2xl sm:mt-0 mt-4">
+        Register
+      </div>
+      <input
+        type="text"
+        placeholder="Enter Name..."
+        className="w-full p-2 text-gray-100 bg-gray-700 rounded-lg mt-4"
+      />
+      <input
+        type="text"
+        placeholder="Enter Username..."
+        className="w-full p-2 text-gray-100 bg-gray-700 rounded-lg mt-4"
+      />
+      <input
+        type="email"
+        placeholder="Enter Email Address..."
+        className="w-full p-2 text-gray-100 bg-gray-700 rounded-lg mt-4"
+      />
+      <input
+        type="password"
+        placeholder="Enter Password..."
+        className="w-full p-2 text-gray-100 bg-gray-700 rounded-lg my-4"
+      />
+      <input
+        type="password"
+        placeholder="Enter Password Again..."
+        className="w-full p-2 text-gray-100 bg-gray-700 rounded-lg mb-4"
+      />
+      <button
+        className="p-2 rounded-lg bg-gray-900 uppercase tracking-wide font-bold text-lg hover:bg-blue-600 focus:bg-blue-600 text-gray-300 sm:w-1/3 w-full mb-2"
+        onClick={() => null}
+      >
+        Create!
+      </button>
+      <div className="sm:text-lg text-md text-gray-400">
+        Already have an account?{' '}
+        <button
+          className="underline text-blue-400 sm:mb-0 mb-4"
+          onClick={() => setIsRegistering(false)}
+        >
+          Login Now!
+        </button>
+      </div>
+    </div>
+  );
+
   return (
     <div className="w-screen h-screen overflow-hidden flex justify-center items-center">
       <img
@@ -23,27 +106,7 @@ export default function LoginForm() {
           />
         </div>
         <div className="h-full sm:w-1/2 w-full bg-gray-800 rounded-r-lg sm:border-l-4 sm:border-gray-600 flex flex-col justify-center">
-          <div className="w-5/6 mx-auto">
-            <div className="font-bold tracking-wide text-white sm:text-4xl text-2xl sm:mt-0 mt-4">
-              Login
-            </div>
-            <input
-              type="text"
-              placeholder="Enter Username..."
-              className="w-full p-2 text-gray-100 bg-gray-700 rounded-lg my-4"
-            />
-            <input
-              type="password"
-              placeholder="Enter Password..."
-              className="w-full p-2 text-gray-100 bg-gray-700 rounded-lg mb-4"
-            />
-            <button
-              className="p-2 rounded-lg bg-gray-900 uppercase tracking-wide font-bold text-lg hover:bg-blue-600 focus:bg-blue-600 text-gray-300 sm:w-1/3 w-full sm:mb-0 mb-4"
-              onClick={() => null}
-            >
-              Let's Go!
-            </button>
-          </div>
+          {isRegistering ? Register : Login}
         </div>
       </div>
     </div>
