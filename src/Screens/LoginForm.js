d6d3ac6ebe4a0687d8b40143cea86615715f5405
Kishan Takoordyal <kishan@kinesis.games>
2020-10-31 08:34:06 +0400

    add state for all inputs on LoginForm + start working on validity / errors

--- src/Screens/LoginForm.js
+++ src/Screens/LoginForm.js
@@ -6,6 +6,84 @@ import logo from '../Assets/images/logo.png';
 export default function LoginForm() {
   const [isRegistering, setIsRegistering] = useState(false);
 
+  const [loginInputs, setLoginInputs] = useState(['', '']);
+
+  const [registerInputs, setRegisterInputs] = useState(['', '', '', '', '']);
+  const [validity, setValidity] = useState([false, false, false, false, false]);
+  const [error, setError] = useState(['', '', '', '', '']);
+
+  const setName = (e) => {
+    e.preventDefault();
+
+    if (e.target.value.length === 0) {
+      updateValidity(0, false);
+      updateError(0, '');
+    } else {
+      updateValidity(0, true);
+    }
+
+    if (!/^[a-zA-Z ]+$/.test(e.target.value)) {
+      updateError(0, 'Should contain only alphabets');
+      updateValidity(0, false);
+    } else {
+      updateError(0, '');
+    }
+
+    updateRegisterInput(0, e.target.value);
+  };
+
+  const updateRegisterInput = (index, val) => {
+    const update = registerInputs.map((e, i) => {
+      if (i === index) {
+        return val;
+      } else {
+        return e;
+      }
+    });
+
+    setRegisterInputs(update);
+  };
+
+  const updateValidity = (index, val) => {
+    const update = validity.map((e, i) => {
+      if (i === index) {
+        return val;
+      } else {
+        return e;
+      }
+    });
+
+    setValidity(update);
+  };
+
+  const updateError = (index, val) => {
+    const update = error.map((e, i) => {
+      if (i === index) {
+        return val;
+      } else {
+        return e;
+      }
+    });
+
+    setError(update);
+  };
+
+  const showError = (index) => {
+    if (error[index].length === 0) {
+      return null;
+    }
+
+    return (
+      <ul className="w-5/6 mx-auto p-2 text-red-400 my-1 list-disc">
+        <li className="sm:text-sm text-xs">{error[index]}</li>
+      </ul>
+    );
+  };
+
+  const classes = {
+    input: 'w-full p-2 text-gray-100 bg-gray-700 rounded-lg mt-2',
+  };
+
   const Login = (
     <div className="w-5/6 mx-auto">
       <div className="font-bold tracking-wide text-white sm:text-4xl text-2xl sm:mt-0 mt-4">
@@ -14,16 +92,32 @@ export default function LoginForm() {
       <input
         type="text"
         placeholder="Enter Username..."
-        className="w-full p-2 text-gray-100 bg-gray-700 rounded-lg my-4"
+        className={classes.input}
+        value={loginInputs[0]}
+        onChange={(e) => {
+          e.persist();
+          setLoginInputs((prev) => [e.target.value, prev[1]]);
+        }}
       />
       <input
         type="password"
         placeholder="Enter Password..."
-        className="w-full p-2 text-gray-100 bg-gray-700 rounded-lg mb-4"
+        className={classes.input}
+        value={loginInputs[1]}
+        onChange={(e) => {
+          e.persist();
+          setLoginInputs((prev) => [prev[0], e.target.value]);
+        }}
       />
       <button
-        className="p-2 rounded-lg bg-gray-900 uppercase tracking-wide font-bold text-lg hover:bg-blue-600 focus:bg-blue-600 text-gray-300 sm:w-1/3 w-full mb-2"
-        onClick={() => null}
+        className={`p-2 rounded-lg bg-gray-900 uppercase tracking-wide font-bold text-lg ${
+          loginInputs.every((v) => v.length > 0)
+            ? 'hover:bg-blue-600 focus:bg-blue-600'
+            : 'opacity-50'
+        } text-gray-300 sm:w-1/3 w-full mt-4 mb-2`}
+        onClick={() =>
+          loginInputs.every((v) => v.length > 0) ? alert('valid') : null
+        }
       >
         Let's Go!
       </button>
@@ -33,7 +127,7 @@ export default function LoginForm() {
           className="underline text-blue-400 sm:mb-0 mb-4"
           onClick={() => setIsRegistering(true)}
         >
-          Register Now!
+          Make One Now!
         </button>
       </div>
     </div>
@@ -47,31 +141,38 @@ export default function LoginForm() {
       <input
         type="text"
         placeholder="Enter Name..."
-        className="w-full p-2 text-gray-100 bg-gray-700 rounded-lg mt-4"
+        className={classes.input}
+        value={registerInputs[0]}
+        onChange={(e) => setName(e)}
       />
+      {showError(0)}
       <input
         type="text"
         placeholder="Enter Username..."
-        className="w-full p-2 text-gray-100 bg-gray-700 rounded-lg mt-4"
+        className={classes.input}
       />
       <input
         type="email"
         placeholder="Enter Email Address..."
-        className="w-full p-2 text-gray-100 bg-gray-700 rounded-lg mt-4"
+        className={classes.input}
       />
       <input
         type="password"
         placeholder="Enter Password..."
-        className="w-full p-2 text-gray-100 bg-gray-700 rounded-lg my-4"
+        className={classes.input}
       />
       <input
         type="password"
         placeholder="Enter Password Again..."
-        className="w-full p-2 text-gray-100 bg-gray-700 rounded-lg mb-4"
+        className={classes.input}
       />
       <button
-        className="p-2 rounded-lg bg-gray-900 uppercase tracking-wide font-bold text-lg hover:bg-blue-600 focus:bg-blue-600 text-gray-300 sm:w-1/3 w-full mb-2"
-        onClick={() => null}
+        className={`p-2 rounded-lg bg-gray-900 uppercase tracking-wide font-bold text-lg ${
+          validity.every((v) => v)
+            ? 'hover:bg-blue-600 focus:bg-blue-600'
+            : 'opacity-50'
+        } text-gray-300 sm:w-1/3 w-full my-4`}
+        onClick={() => (validity.every((v) => v) ? alert('valid') : null)}
       >
         Create!
       </button>
