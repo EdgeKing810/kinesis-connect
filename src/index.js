5b67bd4d7620f992bb0fad516cfb3d197b67843a
Kishan Takoordyal <kt@esokia-webagency.com>
2020-11-19 10:18:17 +0400

    move ws to a dedicated component

--- src/index.js
+++ src/index.js
@@ -4,12 +4,17 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 import App from './App';
+
+import WebSocketWrapper from './WebSocketWrapper';
+
 import { LocalContextProvider } from './Context';
 
 ReactDOM.render(
   <LocalContextProvider>
     <Router>
-      <App />
+      <WebSocketWrapper>
+        <App />
+      </WebSocketWrapper>
     </Router>
   </LocalContextProvider>,
   document.getElementById('root')
